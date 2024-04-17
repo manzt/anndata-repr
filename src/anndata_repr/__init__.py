3190b4f915c5ce03cbbcd4ba82366c44cb2fdb33
Trevor Manz <trevor.j.manz@gmail.com>
2024-04-17 13:15:53 -0400

    feat: Remove variants and auto format anndata

--- src/anndata_repr/__init__.py
+++ src/anndata_repr/__init__.py
@@ -1,5 +1,11 @@
-from ._formatting_html import (  # noqa: F401
-    format_anndata_html_compact,
-    format_anndata_html_complete,
-)
+from ._formatting_html import format_anndata_html  # noqa: F401
 from ._version import __version__  # noqa: F401
+
+try:
+    import anndata
+    anndata.AnnData._repr_html_ = format_anndata_html
+
+except ImportError:
+    import warnings
+
+    warnings.warn("anndata not found. Please install anndata to enable anndata formatting.")
